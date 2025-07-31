c6d96d67ac5e3a0465bed31f69c12240c0420e53
Vaibhav0x <rajjvaibhavv121@gmail.com>
2025-08-01 02:01:32 +0530

    added new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,16 @@ import { image } from "framer-motion/client";
 export const projects = [
   {
     id: 1,
+    title: "Youtube Search Suggestions",
+    description: "A web application that provides real-time search suggestions for YouTube videos using a custom-built API.",
+    tech: ["Python", "Django", "Django Rest Framework", "Tailwind", "ReactJs", "Framer Motion"],
+    image: "📹",
+    github: "https://github.com/Vaibhav0x/youtube-search-frontend",
+    live: "https://yt-suggest-zeta.vercel.app/",
+    featured: true
+  },
+  {
+    id: 2,
     title: "Rweet Django Based Social Media Web App",
     description: "Full-stack application with user authentication, email verification, real-time updates, and a modern UI built with Django and django templates.",
     tech: ["Python", "Django", "Bootstrap", "PostgreSQL", "HTML", "CSS", "SMTP"],
@@ -13,7 +23,7 @@ export const projects = [
     featured: true
   },
   {
-    id: 2,
+    id: 3,
     title: "TransVox Transcription System",
     description: "Desktop Application for audio transcription with features like voice recognition, voice to text conversion ,multi-language translation.",
     tech: ["Python", "QT Designer", "SQLite", "NLP", "Speech Recognition", "PyQt5"],
@@ -24,7 +34,7 @@ export const projects = [
     featured: true
   },
   {
-    id: 3,
+    id: 4,
     title: "BurNCode - Code Editor",
     description: "Collaborative code editor with real-time collaboration, syntax highlighting, and a modern UI built with React and Piston API.",
     tech: ["React", "JavScript", "Chakra UI", "Piston API"],
@@ -35,7 +45,7 @@ export const projects = [
     featured: true
   },
   {
-    id: 4,
+    id: 5,
     title: "FilFusion: Movie Recommendation System",
     description: "AI-powered movie recommendation system with personalized suggestions, user reviews, and a sleek interface built with Streamlit.",
     tech: ["Python", "Machine Learning", "Streamlit"],
@@ -46,7 +56,7 @@ export const projects = [
     featured: false
   },
   {
-    id: 5,
+    id: 6,
     title: "Spotify Clone: Frontend",
     description: "Beautiful weather application with animated backgrounds and detailed forecasts using multiple APIs.",
     tech: ["React", "JavaScript", "Tailwind"],
