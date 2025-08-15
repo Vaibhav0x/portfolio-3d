d97c130cfbe07d37db277c0ccc8f12ede49a9a3d
Vaibhav0x <rajjvaibhavv121@gmail.com>
2025-08-15 13:21:08 +0530

    react app fix api url

--- src/components/ReviewsSection.jsx
+++ src/components/ReviewsSection.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import { FaStar } from "react-icons/fa";
 
-const API_BASE = process.env.NEXT_PUBLIC_REVIEWS_API;
+const API_BASE = process.env.REACT_APP_NEXT_PUBLIC_REVIEWS_API;
+// const API_BASE = "https://reviews-backend-production.up.railway.app";
+
+console.log("API_BASE:", API_BASE);
+
 
 const ReviewsSection = () => {
     const [reviews, setReviews] = useState([]);
@@ -29,6 +33,7 @@ const ReviewsSection = () => {
     const fetchReviews = async () => {
         try {
             const url = `${API_BASE}/reviews`;
+            console.log("Fetching reviews from:", url);
 
             const response = await fetch(url);
 
